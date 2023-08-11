e5e805ebac35481e9287de2ca3e3d8ab555e143e
Mitchell DeJong <61512125+dejongmit@users.noreply.github.com>
2023-08-11 16:20:53 -0400

    Creating Calculator Functions
    Create functions for basic arithmetic, and a operate function to choose
    the correct math function to call.

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("Hello World!");
+let firstNum = 0
+let operator = "";
+let lastNum = 0;
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate (firstNum, operator, lastNum) {
+    switch (operator) {
+        case "+":
+            return add(firstNum,lastNum);
+        case "-":
+            return subtract(firstNum, lastNum);
+        case "*":
+            return multiply(firstNum, lastNum);
+        case "/":
+            return divide(firstNum, lastNum);
+        default:
+            throw new Error("Error: Operator not understood!");
+    }
+}
+
+console.log(operate(5,"+",5));
+console.log(operate(5,"-",5));
+console.log(operate(5,"*",5));
+console.log(operate(5,"/",5));
+console.log(operate(5,"d",5));
