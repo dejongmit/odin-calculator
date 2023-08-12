4cb5f1bd5c0568c51ff2480926ea64e30e0974e1
Mitchell DeJong <61512125+dejongmit@users.noreply.github.com>
2023-08-11 20:57:21 -0400

    Handel left operator:
    Bug fixes for clear and decimal place buttons.
    When there is no operand, do no math and concat in leftVal.

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 // Variables & Page Elements
-let firstNum = 0
+let leftVal = 0;
 let operator = "";
-let lastNum = 0;
+let rightVal = 0;
 let displayVal = "";
 const displaySpan = document.querySelector('.displayVal')
-const digitButtons = document.querySelectorAll('.digit');
 const clearButton = document.querySelector('.clear');
+const pointButton = document.querySelector('.point');
+const digitButtons = document.querySelectorAll('.digit');
+const operandButtons = document.querySelectorAll('.operand');
+
 
 // Functions
-function display(value) {
-    if(displayVal === "0"){
-        displayVal = value;
+function display(int) {
+    if(displayVal === "0"){ //If display is already zero, don't concatenate
+        if(int === ".") { //If incoming value is a decimal, include a leading zero
+            displayVal = "0."
+            displaySpan.textContent = displayVal;
+        } else {
+            displayVal = int.toString();
+            displaySpan.textContent = displayVal;
+        }
+    } else{
+        displayVal += int.toString();
         displaySpan.textContent = displayVal;
-    } else {
-    displayVal += value;
-    displaySpan.textContent = displayVal;
     }
 }
 
@@ -34,16 +42,16 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate (firstNum, operator, lastNum) {
+function operate (oprLeftVal, operator, oprRightVal) {
     switch (operator) {
         case "+":
-            return add(firstNum,lastNum);
+            return add(oprLeftVal, oprRightVal);
         case "-":
-            return subtract(firstNum, lastNum);
+            return subtract(oprLeftVal, oprRightVal);
         case "*":
-            return multiply(firstNum, lastNum);
+            return multiply(oprLeftVal, oprRightVal);
         case "/":
-            return divide(firstNum, lastNum);
+            return divide(oprLeftVal, oprRightVal);
         default:
             throw new Error("Error: Operator not understood!");
     }
@@ -51,11 +59,37 @@ function operate (firstNum, operator, lastNum) {
 
 // Event Listeners
 clearButton.addEventListener('click', () => {
-    displayVal = "";
-    display(0);
+    displayVal = "0" //clear the display, then push a 0 onto it
+    display("0");
+});
+
+pointButton.addEventListener('click', () => { 
+    if(displayVal.includes(".")) { //check to see if a decimal has already been placed
+        return null;
+    } else {
+        display(".")
+    }
 });
 
 digitButtons.forEach((button) => {
-    button.addEventListener('click',() => {display(button.textContent)})
+    button.addEventListener('click', () => {
+        if(operator === "") {
+            display(parseInt(button.textContent));
+            if(displayVal.includes(".")) { //use float if there is a decimal
+                leftVal = parseFloat(displayVal);
+            } else {
+                leftVal = parseInt(displayVal);
+            }
+            console.log(leftVal);
+        } else {
+            operator = ""; 
+            //TO DO: FIGURE OUT WHAT TO DO IF THERE'S MATH SUPPOSED TO HAPPEN
+            // display(button.textContent)
+        }
+    })
+});
+
+operandButtons.forEach((button) => {
+    button.addEventListener('click', () => {operator = button.textContent;})
 });
 
